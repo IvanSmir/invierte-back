ef2862258331a64191e9140d4198dfec2bbe4464
Ivan <jf561428@gmail.com>
2025-06-21 14:54:08 -0300

    fix(cors): update CORS configuration to use allowed origins from environment variable

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-const host = process.env.FRONTEND_URL  || 'http://localhost:3000' || 'http://127.0.0.1:3001';
+const allowedOrigins = [
+  ...(process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : []),
+];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
   app.enableCors({
-    origin: host,
+    origin: allowedOrigins,
     credentials: true,
   });
   app.use(cookieParser());
